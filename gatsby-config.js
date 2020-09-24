aac5d5ddf8e15349f1559fbf9d841de0356f208d
Oswaldo Zárate Cisneros <oswaldo.zarate@wizeline.com>
2020-09-24 11:11:57 -0500

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -71,6 +71,12 @@ module.exports = {
         defer: true,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-178916468-1`,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
